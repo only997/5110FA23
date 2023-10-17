ed3880afe3de719f3e849b53a1f0672b802a5013
koenigm-seattleu <koenigm@seattleu.edu>
2023-10-16 22:34:20 -0700

    Added UT base file JsonFileProductServiceTests.cs to the UnitTest Project

---
+++ UnitTests/Pages/Services/JsonFileProductServiceTests.cs
@@ -0,0 +1,64 @@
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Product.AddRating
+{
+    public class JsonFileProductServiceTests
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region AddRating
+        //[Test]
+        //public void AddRating_InValid_....()
+        //{
+        //    // Arrange
+
+        //    // Act
+        //    //var result = TestHelper.ProductService.AddRating(null, 1);
+
+        //    // Assert
+        //    //Assert.AreEqual(false, result);
+        //}
+
+        // ....
+
+        [Test]
+        public void AddRating_InValid_Product_Null_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(null, 1);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_InValid_Product_Empty_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating("", 1);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        #endregion AddRating
+
+    }
+}
