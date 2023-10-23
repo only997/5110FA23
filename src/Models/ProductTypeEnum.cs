dc69c21e0b2496cb95d79e119aacefe39cae3925
mckeemseattleu <mckeem@seattleu.edu>
2023-10-22 18:24:32 -0700

    Added ProductTypeEnum to have a special category for costumes.

---
+++ src/Models/ProductTypeEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ContosoCrafts.WebSite.Models
+{/// <summary>
+/// ProductTypeEnum is a enum (Class) that will categorize various types
+/// of costumes
+/// </summary>
+    public enum ProductTypeEnum
+    {
+        Undefined = 0,
+        Scary = 1,
+        Kids = 5,
+        Adult = 130,
+        Princess = 55,
+    }
+
+    public static class ProductTypeEnumExtensions
+    {
+        public static string DisplayName(this ProductTypeEnum data)
+        {
+            return data switch
+            {
+                ProductTypeEnum.Scary => "Scary Costumes",
+                ProductTypeEnum.Kids => "Kids",
+                ProductTypeEnum.Adult => "Adult",
+                ProductTypeEnum.Princess => "Princess",
+
+                // Default, Unknown
+                _ => "",
+            };
+        }
+    }
+
+}
