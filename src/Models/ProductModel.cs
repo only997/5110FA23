8e3aae17fd8560160b559a2d198847577b916b03
mckeemseattleu <mckeem@seattleu.edu>
2023-10-26 19:38:16 -0700

    Modified ProductModel.cs to account for new additions to our model

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -19,10 +19,10 @@ namespace ContosoCrafts.WebSite.Models
 
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        public string Quantity { get; set; }
+        public int Quantity { get; set; }
 
         [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-        public int Price { get; set; }
+        public decimal Price { get; set; }
 
         // Store the Comments entered by the users on this product
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
