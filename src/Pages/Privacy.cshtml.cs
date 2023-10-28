abd421f1fda4d390dc4727618fdb4ef3c801eeed
mckeemseattleu <mckeem@seattleu.edu>
2023-10-27 20:42:00 -0700

    Modified Privacy.cshtml.cs to add documentation.

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,23 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// PrivacyModel used for Privacy Page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Logger variable
         private readonly ILogger<PrivacyModel> _logger;
 
+        // Constructor that accepts logger as input
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// OnGet will essentially do nothing 
+        /// </summary>
         public void OnGet()
         {
         }
