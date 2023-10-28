1adec455cb8bb933b70aa3b7086b2e099ae3aac3
mckeemseattleu <mckeem@seattleu.edu>
2023-10-27 20:43:33 -0700

    Modified ProductTypeEnum.cs to add documentation.

--- src/Models/ProductTypeEnum.cs
+++ src/Models/ProductTypeEnum.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 namespace ContosoCrafts.WebSite.Models
-{/// <summary>
-/// ProductTypeEnum is a enum (Class) that will categorize various types
-/// of costumes
-/// </summary>
+{
+    
+    /// <summary>
+    /// ProductTypeEnum is a enum (Class) that will categorize various types
+    /// of costumes
+    /// </summary>
     public enum ProductTypeEnum
     {
         Undefined = 0,
@@ -16,8 +18,18 @@ namespace ContosoCrafts.WebSite.Models
         Princess = 55,
     }
 
+    /// <summary>
+    /// ProductTypeEnumExtensions is a static class with Display functionality
+    /// of costumes
+    /// </summary>
     public static class ProductTypeEnumExtensions
     {
+
+        /// <summary>
+        /// Function of Addrating to a particular productId
+        /// </summary>
+        /// <param name="data">This is a ProductTypeEnum </param>
+        /// <returns></returns>
         public static string DisplayName(this ProductTypeEnum data)
         {
             return data switch
