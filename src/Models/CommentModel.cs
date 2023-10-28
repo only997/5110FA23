605345fff0c6907438757f658336d58fa5929d64
mckeemseattleu <mckeem@seattleu.edu>
2023-10-27 20:44:11 -0700

    Modified CommentModel.cs to add documentation.

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -8,7 +8,7 @@
         // The ID for this comment, use a Guid so it is always unique
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
 
-        // The Comment
+        // The Comment getter and setter
         public string Comment { get; set; }
     }
 }
