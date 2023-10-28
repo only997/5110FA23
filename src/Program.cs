5c8aab692338cfcbc6adb70dca27233dc09a8116
mckeemseattleu <mckeem@seattleu.edu>
2023-10-27 20:41:04 -0700

    Modified Program.cs to add documentation.

--- src/Program.cs
+++ src/Program.cs
@@ -3,13 +3,25 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// Program does startup things associated with Application
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main is the Starting point in application
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// CreateHostBuilder is necessary function for startup
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
